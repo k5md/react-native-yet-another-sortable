70f6b1a076610d144f306b57c3077e9f0b918c9f
k5md <serg.dfc@yandex.ru>
2020-08-08 16:02:18 +0300

    fix issue with overlapping dependencies

--- example/metro.config.js
+++ example/metro.config.js
@@ -6,6 +6,8 @@
  */
 
 const path = require('path');
+const escape = require('escape-string-regexp');
+const blacklist = require('metro-config/src/defaults/blacklist');
 
 module.exports = {
   transformer: {
@@ -16,10 +18,16 @@ module.exports = {
       },
     }),
   },
+  projectRoot: __dirname,
   watchFolders: [
-    path.resolve(__dirname, '../'),
+    path.resolve(__dirname, '..'),
   ],
   resolver: {
+    blacklistRE: blacklist([
+      new RegExp(
+        `^${escape(path.resolve(__dirname, '..', 'node_modules'))}\/.*$`
+      ),
+    ]),
     extraNodeModules: new Proxy(
       {},
       {
