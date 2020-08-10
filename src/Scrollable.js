ea30bccb4ddfbf768254630ee1214248ad45c57d
k5md <serg.dfc@yandex.ru>
2020-08-10 22:27:58 +0300

    separate scroll and sortable functionalities

---
+++ src/Scrollable.js
@@ -0,0 +1,140 @@
+/* eslint-disable eqeqeq */
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+import { shape, number, string, objectOf, arrayOf, func, object } from 'prop-types';
+import { clamp } from 'lodash';
+import SortableGrid from './Sortable';
+
+class ScrollableGrid extends Component {
+  scrollView = React.createRef();
+  scrollOffset = { x: 0, y: 0 };
+  activeBlockOffset = { x: 0, y: 0};
+  keepScrolling = false;
+
+  onGrantBlock = (evt, gestureState, grid) => {
+    const activeBlockPosition = grid.getActiveBlock().origin;
+    const x = activeBlockPosition.x - gestureState.x0;
+    const y = activeBlockPosition.y - gestureState.y0 - this.scrollOffset.y;
+    this.activeBlockOffset = { x, y };
+    return this.props.onGrantBlock(evt, gestureState, grid);
+  };
+
+  onMoveBlock = (evt, gestureState, grid) => {
+    let dragPosition = {
+      x: gestureState.moveX + this.activeBlockOffset.x,
+      y: gestureState.moveY + this.activeBlockOffset.y,
+    };
+    const activeBlock = grid.getActiveBlock();
+    const originalPosition = activeBlock.origin;
+
+    const scrollThreshold = grid.props.blockHeight / 5;
+    const scrollUp = dragPosition.y < scrollThreshold && this.scrollOffset.y > 0;
+    const scrollDown =
+      dragPosition.y > this.layout.height - this.layout.y - scrollThreshold &&
+      dragPosition.y + this.scrollOffset.y + scrollThreshold < grid.gridHeight;
+
+    const scrollBy = (scrollUp * -1 + scrollDown * 1) * grid.props.blockHeight;
+
+    const clampX = x => clamp(x, 0, grid.gridWidth - grid.blockWidth);
+    const clampY = y => clamp(y, 0, grid.gridHeight - grid.props.blockHeight);
+
+    if (!scrollDown && !scrollUp) {
+      const actualDragPosition = {
+        x: clampX(dragPosition.x),
+        y: clampY(dragPosition.y + this.scrollOffset.y + scrollBy),
+      };
+      activeBlock.currentPosition.setValue(actualDragPosition);
+      grid.moveBlock(originalPosition, actualDragPosition);
+      this.keepScrolling = clearInterval(this.keepScrolling);
+    } else {
+      const actualDragPosition = {
+        x: clampX(dragPosition.x),
+        y: clampY(dragPosition.y + this.scrollOffset.y),
+      };
+      activeBlock.currentPosition.setValue(actualDragPosition);
+      grid.moveBlock(originalPosition, actualDragPosition);
+      this.keepScrolling = clearInterval(this.keepScrolling);
+      this.keepScrolling = setInterval(() => {
+        const activeBlock = grid.getActiveBlock();
+        const originalPosition = activeBlock.origin;
+        const actualDragPosition = {
+          x: clampX(dragPosition.x),
+          y: clampY(dragPosition.y + this.scrollOffset.y + scrollBy),
+        };
+        this.scrollView.current.scrollTo({ y: this.scrollOffset.y + scrollBy });
+        activeBlock.currentPosition.setValue(actualDragPosition);
+        grid.moveBlock(originalPosition, actualDragPosition);
+      }, 250);
+    }
+
+    this.props.onMoveBlock(evt, gestureState, grid);
+    return true;
+  };
+
+  onReleaseBlock = (evt, gestureState, grid) => {
+    this.panCapture = false;
+    this.keepScrolling = clearInterval(this.keepScrolling);
+    return this.props.onReleaseBlock(evt, gestureState, grid);
+  };
+
+  onLayout = ({ nativeEvent }) => {
+    this.layout = nativeEvent.layout;
+  };
+
+  onScroll = ({ nativeEvent }) => {
+    this.scrollOffset = nativeEvent.contentOffset;
+  };
+
+  activateDrag = () => {
+    this.panCapture = true;
+  };
+
+  render = () => (
+    <ScrollView
+      ref={this.scrollView}
+      onLayout={this.onLayout}
+      onScroll={this.onScroll}
+      scrollEnabled={!this.panCapture}
+      showsVerticalScrollIndicator={false}
+      canCancelContentTouches={false}
+    >
+      <SortableGrid
+        {...this.props}
+        onGrantBlock={this.onGrantBlock}
+        onMoveBlock={this.onMoveBlock}
+        onReleaseBlock={this.onReleaseBlock}
+        activateDrag={this.activateDrag}
+      />
+    </ScrollView>
+  );
+}
+
+ScrollableGrid.propTypes = {
+  transitionDuration: number,
+  columns: number,
+  activationThreshold: number,
+  order: arrayOf(string).isRequired,
+  children: arrayOf(
+    shape({
+      key: string,
+    }),
+  ),
+  onDragRelease: func,
+  blockHeight: number.isRequired,
+  style: object,
+  onGrantBlock: func,
+  onReleaseBlock: func,
+  onMoveBlock: func,
+};
+
+ScrollableGrid.defaultProps = {
+  transitionDuration: 200,
+  columns: 4,
+  activationThreshold: 200,
+  style: {},
+  onGrantBlock: () => {},
+  onReleaseBlock: () => {},
+  onMoveBlock: () => {},
+};
+
+module.exports = ScrollableGrid;
---
+++ src/Sortable.js
@@ -0,0 +1,233 @@
+/* eslint-disable eqeqeq */
+import React, { Component } from 'react';
+import { Animated, TouchableWithoutFeedback, PanResponder, View, ScrollView } from 'react-native';
+import { sortBy, noop, clamp } from 'lodash';
+import { shape, number, string, objectOf, arrayOf, func, object } from 'prop-types';
+import { animateTiming, animateSpring, getDistance } from './utils';
+import { StyleSheet } from 'react-native';
+
+class SortableGrid extends Component {
+  itemOrder = {};
+  blockPositions = {};
+  activeBlock = null;
+  gridHeight = 0;
+  gridWidth = 0;
+  blockWidth = 0;
+  panCapture = false;
+  panResponder = PanResponder.create({
+    onStartShouldSetPanResponderCapture: () => false,
+    onMoveShouldSetPanResponder: () => this.panCapture,
+    onMoveShouldSetPanResponderCapture: () => this.panCapture,
+    onPanResponderGrant: (evt, gestureState) => this.onGrantBlock(evt, gestureState),
+    onPanResponderMove: (evt, gestureState) => this.onMoveBlock(evt, gestureState),
+    onPanResponderRelease: (evt, gestureState) => this.onReleaseBlock(evt, gestureState),
+  });
+
+  activeBlockOffset = null;
+
+  startDragWiggle = new Animated.Value(0);
+
+  UNSAFE_componentWillUpdate = (nextProps) => {
+    this.gridHeight = nextProps.blockHeight * Math.ceil(nextProps.children.length / nextProps.columns);
+    this.blockWidth = (this.blockWidth * this.props.columns) / nextProps.columns;
+
+    const oldBlockPositions = Object.keys(this.blockPositions);
+    oldBlockPositions.forEach(
+      key => !nextProps.children.find(child => child.key == key) && delete this.blockPositions[key],
+    );
+
+    this.itemOrder = {};
+
+    const toBeAnimated = [];
+
+    nextProps.order.forEach((key, index) => {
+      this.itemOrder[key] = { key, order: index };
+      const blockPosition = {
+        x: (index % nextProps.columns) * this.blockWidth,
+        y: Math.floor(index / nextProps.columns) * nextProps.blockHeight,
+      };
+    
+      if (!this.blockPositions[key]) {
+        this.blockPositions[key] = {
+          currentPosition: new Animated.ValueXY(blockPosition),
+          origin: blockPosition,
+        };
+      } else {
+        this.getBlock(key).origin = blockPosition;
+        // this.getBlock(key).currentPosition.setValue(blockPosition);
+        toBeAnimated.push(animateTiming(this.getBlock(key).currentPosition, blockPosition, this.props.transitionDuration * 4));
+      }
+    });
+    Animated.parallel(toBeAnimated, { stopTogether: false }).start();
+  };
+
+  onGrantBlock = (evt, gestureState) => {
+    const override = this.props.onGrantBlock(evt, gestureState, this);
+    if (override) {
+      return;
+    }
+    const activeBlockOrigin = this.getActiveBlock().origin;
+    this.activeBlockOffset = {
+      x: activeBlockOrigin.x - gestureState.x0,
+      y: activeBlockOrigin.y - gestureState.y0,
+    };
+  };
+
+  onMoveBlock = (evt, gestureState) => {
+    const override = this.props.onMoveBlock(evt, gestureState, this);
+    if (override) {
+      return;
+    }
+    const dragPosition = {
+      x: gestureState.moveX + this.activeBlockOffset.x,
+      y: gestureState.moveY + this.activeBlockOffset.y,
+    };
+    const activeBlock = this.getActiveBlock();
+    const originalPosition = activeBlock.origin;
+    const currentPosition = activeBlock.currentPosition;
+
+    const actualDragPosition = {
+      x: clamp(dragPosition.x, 0, this.gridWidth - this.blockWidth),
+      y: clamp(dragPosition.y, 0, this.gridHeight - this.props.blockHeight),
+    };
+    currentPosition.setValue(actualDragPosition);
+    this.moveBlock(originalPosition, actualDragPosition);
+  };
+
+  onReleaseBlock = (evt, gestureState) => {
+    const override = this.props.onReleaseBlock(evt, gestureState, this);
+    if (override) {
+      return;
+    }
+    this.panCapture = false;
+
+    const activeBlock = this.getActiveBlock();
+    const currentPosition = activeBlock.currentPosition;
+    const originalPosition = activeBlock.origin;
+
+    animateTiming(currentPosition, originalPosition, this.props.transitionDuration, () => {
+      const itemOrder = sortBy(this.itemOrder, item => item.order).map(item => item.key);
+      this.props.onDragRelease(itemOrder);
+      this.activeBlock = null;
+      this.forceUpdate();
+    });  
+  };
+
+  activateDrag = key => () => {
+    const override = this.props.activateDrag(this);
+    if (override) {
+      return;
+    }
+    this.panCapture = true;
+    animateSpring(this.startDragWiggle, 10, 0, () => {});
+    
+    this.activeBlock = key;
+    this.forceUpdate();
+  };
+
+  moveBlock = (originalPosition, currentPosition) => {
+    const activeBlock = this.getActiveBlock();
+    let closest = this.activeBlock;
+    let closestDistance = getDistance(currentPosition, originalPosition);
+
+    for (let key in this.blockPositions) {
+      const block = this.blockPositions[key];
+      const blockPosition = block.origin;
+      const distance = getDistance(currentPosition, blockPosition);
+      if (distance < closestDistance && distance < this.blockWidth) {
+        closest = key;
+        closestDistance = distance;
+      }
+    }
+
+    if (closest == this.activeBlock) {
+      return;
+    }
+
+    const closestBlock = this.getBlock(closest);
+    animateTiming(closestBlock.currentPosition, activeBlock.origin);
+    activeBlock.origin = closestBlock.origin;
+    closestBlock.origin = originalPosition;
+
+    const tempOrder = this.itemOrder[this.activeBlock].order;
+    this.itemOrder[this.activeBlock].order = this.itemOrder[closest].order;
+    this.itemOrder[closest].order = tempOrder;
+  };
+
+  getActiveBlock = () => this.blockPositions[this.activeBlock];
+
+  getBlock = key => this.blockPositions[key];
+
+  blockPositionsSet = () => Object.keys(this.blockPositions).length == this.props.children.length;
+
+  onGridLayout = ({ nativeEvent }) => {
+    this.gridWidth = nativeEvent.layout.width;
+    this.blockWidth = nativeEvent.layout.width / this.props.columns;
+    this.forceUpdate();
+  };
+
+  getGridStyle = () => [
+    styles.grid,
+    this.blockPositionsSet() && { height: this.gridHeight + this.props.blockHeight },
+  ];
+
+  getBlockStyle = key => [
+    {
+      width: this.blockWidth,
+      height: this.props.blockHeight,
+      justifyContent: 'center',
+    },
+    this.blockPositionsSet() && {
+      position: 'absolute',
+      transform: [
+        ...this.getBlock(key).currentPosition.getTranslateTransform(),
+        {
+          rotate:
+            this.activeBlock == key
+              ? this.startDragWiggle.interpolate({
+                  inputRange: [0, 360],
+                  outputRange: ['0 deg', '360 deg'],
+                })
+              : '0deg',
+        },
+      ],
+    },
+    this.activeBlock == key && { zIndex: 1 },
+  ];
+
+  renderEntry = item => (
+    <Animated.View key={item.key} style={this.getBlockStyle(item.key)} {...this.panResponder.panHandlers}>
+      <TouchableWithoutFeedback
+        style={styles.container}
+        delayLongPress={this.props.activationTreshold}
+        onLongPress={item.inactive ? noop : this.activateDrag(item.key)}
+      >
+        <View style={styles.cell}>
+          <View style={styles.container}>{item}</View>
+        </View>
+      </TouchableWithoutFeedback>
+    </Animated.View>
+  );
+
+  render = () => (
+    <Animated.View style={this.getGridStyle()} onLayout={this.onGridLayout}>
+      {this.props.children.map(this.renderEntry)}
+    </Animated.View>
+  );
+}
+
+module.exports = SortableGrid;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  grid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  cell: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
